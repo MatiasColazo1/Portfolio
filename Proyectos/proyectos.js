f776ecdfb7f71915efd2dfc25841b36479beb562
MatiasColazo1 <maty_colazo@hotmail.com>
2023-03-07 23:37:05 -0300

    navbar

---
+++ Proyectos/proyectos.js
@@ -0,0 +1,21 @@
+// Selecciono el boton por su id
+const botonModo = document.getElementById("boton-modo")
+
+// Agregar un controlador de eventos para cuando se haga clic en el botón
+botonModo.addEventListener("click", function() {
+    // Obtener el elemento body
+    const cuerpo = document.body;
+  
+    // Si el cuerpo tiene la clase "modo-claro", cambiar a modo oscuro
+    if (cuerpo.classList.contains("modo-claro")) {
+      cuerpo.classList.remove("modo-claro");
+      cuerpo.classList.add("modo-oscuro");
+      botonModo.textContent = "Modo claro";
+    } else {
+      // De lo contrario, cambiar a modo claro
+      cuerpo.classList.remove("modo-oscuro");
+      cuerpo.classList.add("modo-claro");
+      botonModo.textContent = "Modo oscuro";
+    }
+  });
+
