23c29748020e0b93f043ca7dbabe92cf5dc0a2d3
MatiasColazo1 <maty_colazo@hotmail.com>
2023-04-12 18:45:50 -0300

    menu sticky

--- Inicio/Inicio.js
+++ Inicio/Inicio.js
@@ -1,23 +1,4 @@
-// Selecciono el boton por su id
-const botonModo = document.getElementById("boton-modo")
 
-// Agregar un controlador de eventos para cuando se haga clic en el botón
-botonModo.addEventListener("click", function() {
-    // Obtener el elemento body
-    const cuerpo = document.body;
-  
-    // Si el cuerpo tiene la clase "modo-claro", cambiar a modo oscuro
-    if (cuerpo.classList.contains("modo-claro")) {
-      cuerpo.classList.remove("modo-claro");
-      cuerpo.classList.add("modo-oscuro");
-      botonModo.textContent = "Modo claro";
-    } else {
-      // De lo contrario, cambiar a modo claro
-      cuerpo.classList.remove("modo-oscuro");
-      cuerpo.classList.add("modo-claro");
-      botonModo.textContent = "Modo oscuro";
-    }
-  });
 
 const typed = new Typed('.typed', {
   strings: ['<i class="tipeo">Hola, soy Matías!</i><br>Desarrollador web<br>Full Stack'],
