d05f4dfe30dde8923069a06d67134e0491af8441
MatiasColazo1 <maty_colazo@hotmail.com>
2023-07-09 21:54:44 -0300

    arreglos

--- Inicio/Inicio.js
+++
@@ -1,9 +0,0 @@
-
-
-const typed = new Typed('.typed', {
-  strings: ['<i class="tipeo">Hola, soy Matías!</i><br>Desarrollador web<br>Full Stack'],
-  typeSpeed: 75,
-  backSpeed:40
-});
-
-//tecnologias
---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const typed = new Typed('.typed', {
+    strings: ['<i class="tipeo">Hola, soy Matías!</i><br>Desarrollador web<br>Full Stack'],
+    typeSpeed: 75,
+    backSpeed:40
+  });
+  
+  //tecnologias
+  
