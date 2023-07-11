1af27c513b242882a6bc8b83da205b383d3f08fe
MatiasColazo1 <maty_colazo@hotmail.com>
2023-07-11 16:07:29 -0300

    arreglos

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 
-const typed = new Typed('.typed', {
-    strings: ['<i class="tipeo">Hola, soy Matías!</i><br>Desarrollador web<br>Full Stack'],
-    typeSpeed: 75,
-    backSpeed:40
-  });
-  
+const typedHello = new Typed('.typed-hello', {
+  strings: ['<i class="tipeo">Hola, soy Matías!</i>'],
+  typeSpeed: 75,
+  showCursor: false,
+  onComplete: function() {
+    typedLoop.start();
+  }
+});
+
+const typedLoop = new Typed('.typed-loop', {
+  strings: ['Desarrollador web<br>Full Stack', 'QA Analyst'],
+  typeSpeed: 75,
+  backSpeed: 40,
+  loop: true,
+  loopCount: Infinity
+});
+
   //tecnologias
-  
