8491355b5c462e9d93b0b211ec0ba42d9807fe42
MatiasColazo1 <maty_colazo@hotmail.com>
2023-07-17 12:15:23 -0300

    arreglos

--- Proyectos/proyectos.js
+++ Proyectos/proyectos.js
@@ -1,26 +1,10 @@
-// Selecciono el boton por su id
-const botonModo = document.getElementById("boton-modo")
-
-// Agregar un controlador de eventos para cuando se haga clic en el botón
-botonModo.addEventListener("click", function() {
-    // Obtener el elemento body
-    const cuerpo = document.body;
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeBtn = document.getElementById('dark-mode');
+    const body = document.body;
   
-    // Si el cuerpo tiene la clase "modo-claro", cambiar a modo oscuro
-    if (cuerpo.classList.contains("modo-claro")) {
-      cuerpo.classList.remove("modo-claro");
-      cuerpo.classList.add("modo-oscuro");
-      botonModo.textContent = "Modo claro";
-    } else {
-      // De lo contrario, cambiar a modo claro
-      cuerpo.classList.remove("modo-oscuro");
-      cuerpo.classList.add("modo-claro");
-      botonModo.textContent = "Modo oscuro";
-    }
+    darkModeBtn.addEventListener('click', function() {
+      body.classList.toggle('dark-mode');
+    });
   });
-
-
-
-
-
+  
   
--- SobreMi/sobremi.js
+++ SobreMi/sobremi.js
@@ -1,21 +1,6 @@
-// Selecciono el boton por su id
-const botonModo = document.getElementById("boton-modo")
+const darkModeButton = document.getElementById('dark-mode');
+const body = document.body;
 
-// Agregar un controlador de eventos para cuando se haga clic en el botón
-botonModo.addEventListener("click", function() {
-    // Obtener el elemento body
-    const cuerpo = document.body;
-  
-    // Si el cuerpo tiene la clase "modo-claro", cambiar a modo oscuro
-    if (cuerpo.classList.contains("modo-claro")) {
-      cuerpo.classList.remove("modo-claro");
-      cuerpo.classList.add("modo-oscuro");
-      botonModo.textContent = "Modo claro";
-    } else {
-      // De lo contrario, cambiar a modo claro
-      cuerpo.classList.remove("modo-oscuro");
-      cuerpo.classList.add("modo-claro");
-      botonModo.textContent = "Modo oscuro";
-    }
-  });
-
+darkModeButton.addEventListener('click', function() {
+  body.classList.toggle('dark-mode');
+});
